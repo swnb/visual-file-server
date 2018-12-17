13db01c24d9cfe3f1afec213ccbe4e84a8032579
swnb <1786718956@qq.com>
2018-12-17 10:28:57 +0800

    feature (compress) : add compress function

--- routine/routine.go
+++ routine/routine.go
@@ -2,18 +2,21 @@ package routine
 
 import "sync"
 
+// WaitGroup is syntax suger for sync.WaitGroup
 type WaitGroup struct {
 	Wg sync.WaitGroup
 }
 
+// Go run routine with wait-group count
 func (wg *WaitGroup) Go(fn func(...interface{}), arg ...interface{}) {
 	wg.Wg.Add(1)
 	go func() {
-		fn(arg)
+		fn(arg...)
 		wg.Wg.Done()
 	}()
 }
 
+// Wait wait utill all routine dead
 func (wg *WaitGroup) Wait() {
 	wg.Wg.Wait()
 }
